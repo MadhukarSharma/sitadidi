cc1b093e39b90db455e246c9cac26ac517a23bd7
bimal <pandeybi125@gmail.com>
2017-06-24 12:42:13 +0545

    galaries table migration written

--- database/migrations/2017_06_24_062651_create_galaries_table.php
+++ database/migrations/2017_06_24_062651_create_galaries_table.php
@@ -13,7 +13,14 @@ class CreateGalariesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('galaries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('image_path');
+            $table->string('thumbnail_path');
+            $table->string('status');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +30,6 @@ class CreateGalariesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('galaries');
     }
 }
