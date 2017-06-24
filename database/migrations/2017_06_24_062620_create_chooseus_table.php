0d9aab4a1dcec871454dd72a7c724b00e9dee7ad
bimal <pandeybi125@gmail.com>
2017-06-24 13:14:55 +0545

    error corrected

--- database/migrations/2017_06_24_062620_create_chooseus_table.php
+++ database/migrations/2017_06_24_062620_create_chooseus_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateChoosesTable extends Migration
+class CreateChooseUsTable extends Migration
 {
     /**
      * Run the migrations.
